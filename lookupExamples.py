14235d58fe2e67e4d516a5472bda789a9237f893
John William Kustin <kustinj@stanford.edu>
2021-03-23 01:17:23 -0700

    finished characterization on 1.8V nmos and HVT pmos with width=0.65

--- lookupExamples.py
+++ lookupExamples.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-lk.init('mosPDK_90_W1u.dat')
+lk.init('mosSKY130__W650000.0u.dat')
 VGS = np.arange(0, 1, 0.1)
 mid = lk.lookup('nfet', 'id', vds=0.9, vsb=0, l=0.5, vgs=VGS)
 
