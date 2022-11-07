8948a5fb1852cc322dc7966d6315d69798fd4ff5
johnkustin <johnkustin@gmail.com>
2022-11-06 21:03:53 -0500

    basic

--- pyMOSChar/charMOS.py
+++ pyMOSChar/charMOS.py
@@ -79,16 +79,16 @@ class charMOS:
         for i in range(len(sizes[0])):
                     for ivsb, vsb in enumerate(data["vsb"]):
                         idx = f'{i}d{ivsb}d{tab1[type]}'
-                        netlistHandler.write(f'vdsd{idx}  nDraind{idx} 0 dc 0\n')
-                        netlistHandler.write(f'vgsd{idx}  nGated{idx}  0 dc 0\n')
-                        netlistHandler.write(f'vbsd{idx}  nBulkd{idx}  0 dc {-vsb}\n')
+                        netlistHandler.write(f'vdsd{idx}  {tab1[type]}Draind{idx} 0 dc 0\n')
+                        netlistHandler.write(f'vgsd{idx}  {tab1[type]}Gated{idx}  0 dc 0\n')
+                        netlistHandler.write(f'vbsd{idx}  {tab1[type]}Bulkd{idx}  0 dc {-vsb}\n')
                         netlistHandler.write("\n")
                         model = self.settings[tab[type]]
                         length = sizes[0][i]
                         width = sizes[1][i]
                         netlistHandler.write(f"x{tab1[type]}d{idx} {tab1[type]}Draind{idx} {tab1[type]}Gated{idx} 0 {tab1[type]}Bulkd{idx} {model} L={length*1e-6} W={width*1e-6}\n")
                         netlistHandler.write("\n")
-                        netlistHandler.write(f"dc vgs{idx} 0 {0} {1} vds 0 {2} {3}\n".format(self.settings['vgsMax'], self.settings['vgsStep'], self.settings['vdsMax'], self.settings['vdsStep']))
+                        netlistHandler.write(f"dc vgsd{idx} 0 {0} {1} vdsd{idx} 0 {2} {3}\n".format(self.settings['vgsMax'], self.settings['vgsStep'], self.settings['vdsMax'], self.settings['vdsStep']))
                         netlistHandler.write("\n")
 
     def genNetlistNngspice(self, fName='charNMOS.net'):
