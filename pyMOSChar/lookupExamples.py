65be14f9fae6a4da7ba5b2b15358d426af0e40d6
johnkustin <johnkustin@gmail.com>
2022-11-20 11:45:33 -0500

    trying something

--- pyMOSChar/lookupExamples.py
+++ pyMOSChar/lookupExamples.py
@@ -29,7 +29,9 @@ for idx in range(len(types)):
     VGSmesh = np.array(np.meshgrid(VGS, lengths))
     for i, k in enumerate(K_norm):
         idxx = np.squeeze(indc[i])
-        axK.plot(VGS[idxx]-vt[i][indc[i]], np.log10(k[indc[i]]), label=lengths[i])
+        x_axis = np.squeeze(VGS[idxx]-vt[i][indc[i]])
+        y_axis = np.squeeze(np.log10(k[indc[i]]))
+        axK.plot(x_axis, y_axis, label=lengths[i])
     axK.legend()
     fig, ax1 = plt.subplots(3)
 
